f32dfc477a804e9117ace5aa95c8e68143803f4e
User <kristall@ua.fm>
2021-07-08 01:15:44 +0300

    Repositories injection

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Interfaces\FilmRepositoryInterface;
+use App\Repositories\Interfaces\GenderRepositoryInterface;
+use App\Repositories\Interfaces\HomeworldRepositoryInterface;
+use App\Repositories\Interfaces\PersonRepositoryInterface;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +14,21 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $personRepository;
+    protected $filmRepository;
+    protected $homeworldRepository;
+    protected $genderRepository;
+
+    public function __construct(
+        PersonRepositoryInterface $personRepository,
+        FilmRepositoryInterface $filmRepository,
+        GenderRepositoryInterface $genderRepository,
+        HomeworldRepositoryInterface $homeworldRepository
+    ) {
+        $this->personRepository = $personRepository;
+        $this->filmRepository = $filmRepository;
+        $this->genderRepository = $genderRepository;
+        $this->homeworldRepository = $homeworldRepository;
+    }
 }
