c31185983665d36a4e55591228b2fddfe793e6d2
User <kristall@ua.fm>
2021-07-08 00:33:27 +0300

    Comments in the repository added

--- app/Repositories/HomeworldRepository.php
+++ app/Repositories/HomeworldRepository.php
@@ -7,19 +7,38 @@ namespace App\Repositories;
 use App\Models\Film;
 use App\Models\Homeworld;
 use App\Repositories\Interfaces\HomeworldRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
+
+/**
+ * Class HomeworldRepository
+ * @package App\Repositories
+ */
 class HomeworldRepository implements HomeworldRepositoryInterface
 {
+    /**
+     * Gets all the homeworlds from the DB
+     * @return Homeworld[]|Collection
+     */
     public function getAll()
     {
         return Homeworld::all();
     }
 
+    /**
+     * Adds all the passed homeworld instances to the DB
+     * @param array $homeworldsArray homeworld instances to add
+     */
     public function addAll(array $homeworldsArray)
     {
         Homeworld::insertOrIgnore($homeworldsArray);
     }
 
+    /**
+     * Gets the only homeworld by the specified id
+     * @param int $id  id to get a homeworld
+     * @return mixed
+     */
     public function getOneById(int $id)
     {
         return Homeworld::findOrFail($id);
--- app/Repositories/PersonRepository.php
+++ app/Repositories/PersonRepository.php
@@ -7,29 +7,58 @@ namespace App\Repositories;
 use App\Models\Person;
 use App\Repositories\Interfaces\PersonRepositoryInterface;
 
+/**
+ * Class PersonRepository
+ * @package App\Repositories
+ */
 class PersonRepository implements PersonRepositoryInterface
 {
-
+    /**
+     * Gets All the people by specified order
+     * @param string $columnName order parameter
+     * @param string $orderDirection order direction
+     * @return mixed
+     */
     public function getAllByOrder(string $columnName, string $orderDirection)
     {
         return Person::orderBy($columnName, $orderDirection);
     }
 
+    /**
+     * Gets all the people by the specified column name and value
+     * @param string $columnName Column name
+     * @param string $value column value
+     * @return mixed
+     */
     public function getAllByColumn(string $columnName, string $value)
     {
         return Person::where($columnName, $value);
     }
 
+    /**
+     * Gets the only person by the specified id
+     * @param int $id person's id to get
+     * @return mixed
+     */
     public function getOneById(int $id)
     {
         return Person::findOrFail($id);
     }
 
+    /**
+     * Gets the only person by the specified Name value
+     * @param string $value Name's value
+     * @return mixed
+     */
     public function getOneByName(string $value)
     {
         return Person::firstWhere('name', $value);
     }
 
+    /**
+     * Gets all the people from the DB
+     * @param array $peopleArray
+     */
     public function addAll(array $peopleArray)
     {
         Person::insertOrIgnore($peopleArray);
