b4ccdac324ace57035917f6b1bbcfd765690188f
User <kristall@ua.fm>
2021-07-07 23:27:21 +0300

    Films migration added

---
+++ database/migrations/2021_06_16_101243_create_films_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Database\Seeders\FilmSeeder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
+        });
+
+        Artisan::call('db:seed', ['--class' => FilmSeeder::class]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
