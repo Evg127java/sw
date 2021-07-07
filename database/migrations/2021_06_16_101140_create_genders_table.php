7f895ecf2c4ff5ef05d225898fd388b42891d713
User <kristall@ua.fm>
2021-07-07 23:26:35 +0300

    Genders migration added

---
+++ database/migrations/2021_06_16_101140_create_genders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Database\Seeders\GenderSeeder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGendersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genders', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->unique();
+            $table->timestamps();
+        });
+
+        Artisan::call('db:seed', ['--class' => GenderSeeder::class]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('genders');
+    }
+}
