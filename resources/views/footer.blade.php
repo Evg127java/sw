d547c03e8f137f99e5552c12b180826e6390d7a3
User <kristall@ua.fm>
2021-07-08 21:48:07 +0300

    Footer view added

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,68 @@
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-10 px-0 border-top">
+        <footer class="bg-light text-center text-lg-start">
+            <section class="mb-0">
+                <!-- Facebook -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                ><i class="fab fa-facebook-f"></i
+                    ></a>
+
+                <!-- Twitter -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                ><i class="fab fa-twitter"></i
+                    ></a>
+
+                <!-- Google -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                ><i class="fab fa-google"></i
+                    ></a>
+
+                <!-- Instagram -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                ><i class="fab fa-instagram"></i
+                    ></a>
+
+                <!-- Linkedin -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                ><i class="fab fa-linkedin"></i
+                    ></a>
+                <!-- Github -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                ><i class="fab fa-github"></i
+                    ></a>
+            </section>
+            <!-- Copyright -->
+            <div class="text-center pb-2 bg-light">
+                © 2021 Made by
+                <a class="text-dark" href="https://github.com/Evg127java/"><strong>Evgen Kondratyuk</strong></a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        </div>
+    </div>
+</div>
