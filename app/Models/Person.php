ac5f7f5505b043498d900f1c9629788386b192bf
User <kristall@ua.fm>
2021-07-11 07:38:52 +0300

    Person's fields processing added

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Http\Requests\PersonFormRequest;
 use Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Storage;
 
 /**
  * Class Person
@@ -20,6 +22,17 @@ class Person extends Model
     protected $fillable = ['name', 'gender_id', 'hair_color', 'mass', 'height', 'homeworld_id', 'birth_year', 'url'];
 
     use HasFactory;
+
+    protected $attributes = [
+        'name' => 'name',
+        'height' => 0,
+        'mass' => 0,
+        'hair_color' => 'n/a',
+        'birth_year' => 0,
+        'url' => 'none',
+        'gender_id' => 1,
+        'homeworld_id' => 1,
+    ];
 
     /**
      * Each person is related (belongs to) to only one gender
@@ -47,4 +60,29 @@ class Person extends Model
     {
         return $this->belongsToMany(Film::class);
     }
+
+    public static function createNewPerson(PersonFormRequest $request)
+    {
+        $defaultPerson = self::create();
+        $defaultPerson->updatePerson($request);
+    }
+
+    public function updatePerson(PersonFormRequest $request)
+    {
+        $this->update($request->validationData());
+
+        $this->updateFilmsForPerson();
+        $this->updateImagesForPerson();
+
+        if ($this->isDirty()) {
+            $this->save();
+        }
+    }
+
+    public static function deletePersonById(int $id)
+    {
+        Storage::deleteDirectory('images/' . $id);
+        Person::destroy($id);
+    }
+
 }
