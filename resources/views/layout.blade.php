22d172a34d0b3d3c1ccc706375b936f13589ddf5
User <kristall@ua.fm>
2021-07-22 10:49:54 +0300

    Favico added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="#">
+    <link rel="shortcut icon" href="{{ asset('images/fav/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/fav/favicon.png') }}">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
