7680530292247acc5aa2c6ade30397e0e6feea5b
User <kristall@ua.fm>
2021-09-11 21:07:05 +0300

    Images and favicon added for front

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{ asset('images/fav/favicon.ico') }}">
-    <link rel="icon" type="image/png" href="{{ asset('images/fav/favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('img/fav/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/fav/favicon.png') }}">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/app.css">
